1555ae85b4fdb74e9e128876b0ec252b18f478e9
stnos <s.nos@severex.io>
2019-10-22 17:44:52 +0300

    Update
    ++ up Unity Version
    ++ add temp Dust effect
    ++ add temp UI

---
+++ Assets/2_NewHelicopter/DustEffects/DustAirController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace NewHelicopter
+{
+    public class DustAirController: MonoBehaviour
+    {
+        public ParticleSystem DustParticleSystem;
+        public float MaxDustDistance = 10f;
+
+        [HideInInspector]
+        public float _intensity = 1f;
+        public float Intensity
+        {
+            get { return _intensity; }
+            set
+            {
+                _intensity = Mathf.Clamp01(value);
+                color.a = _intensity;
+            }
+        }
+
+        [HideInInspector]
+        public float _sensitive = 0.2f;
+
+        public float Sensitive
+        {
+            get { return _sensitive; }
+            set { _sensitive = Mathf.Clamp01(value); }
+        }
+
+        private Color color;
+        private float engine;
+        private void Start()
+        {
+        }
+        public void ProgressEngineValue(float engineValue)
+        {
+            engine = engineValue;
+        }
+
+        [System.Obsolete]
+        public void VisualizeDustGround(float distance, Vector3 point)
+        {
+            //DustParticleSystem.gameObject.SetActive(distance < MaxDustDistance);
+
+            //if (DustParticleSystem.gameObject.activeSelf)
+            //{
+                DustParticleSystem.transform.position = point;
+                var distanceFactor = Mathf.Clamp01(distance / MaxDustDistance);
+                
+                DustParticleSystem.startSpeed = 1-distanceFactor;
+                DustParticleSystem.startLifetime = (1-distanceFactor)*2;
+                // var c = color;
+                // c.a = Mathf.Lerp(c.a * engine, c.a * Sensitive, distanceFactor);
+                // DustParticleSystem.startColor = c;
+            //}
+
+        }
+    }
+}
---
+++ Assets/2_NewHelicopter/_UIView/UI/UIViewController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace NewHelicopter
+{
+    public class UIViewController : MonoBehaviour
+    {
+        public GameObject PlayerUI;
+        public GyroscopeViewController GyroscopeView;
+
+        [Header("FlightView")]
+        public Text UpDragView;
+        public Text HeigthUpView;
+        public Text HeigthView;
+        public Text EngineForceView;
+
+        public static UIViewController runtime;
+
+        void Awake()
+        {
+            runtime = this;
+            //CanvasGroup.alpha = 0;
+        }
+
+        public void SetPlayer(Transform player)
+        {
+            PlayerUI.SetActive(true);
+            GyroscopeView.SetPlayer(player);
+        }
+
+      
+        public void DeletePlayer(Transform player)
+        {
+            GyroscopeView.DeletePlayer(player);
+        }
+
+        void Start()
+        {
+            ShowInfoPanel(false);
+        }
+
+
+        private void ShowInfoPanel(bool isShow)
+        {
+            EngineForceView.gameObject.SetActive(!isShow);
+            /*  RestartButton.SetActive(!isShow);
+                 InfoButton.SetActive(!isShow);
+                    InfoPanel.SetActive(isShow);*/
+        }
+
+        public void ShowInfo()
+        {
+            ShowInfoPanel(true);
+        }
+
+        public void HideInfo()
+        {
+            ShowInfoPanel(false);
+        }
+
+        public void RestartGame()
+        {
+            // Application.LoadLevel("Main");
+        }
+    }
+}
