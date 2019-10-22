6fb5aec17c367fe875ab0b542e62cd917a394431
stnos <s.nos@severex.io>
2019-10-22 18:01:17 +0300

    ++ Add universal RadarSystem

---
+++ Assets/2_NewHelicopter/_RadarSystem/AdventureBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class AdventureBehaviour : MonoBehaviour
+{
+    public Action<AdventureBehaviour> OnEnableAction;
+    public Action<AdventureBehaviour> OnDisableAction;
+    public Action<AdventureBehaviour> OnDestroyAction;
+
+    protected virtual void OnEnableObject()
+    {
+        if (OnEnableAction != null)
+            OnEnableAction(this);
+    }
+
+    protected virtual void OnDisableObject()
+    {
+        if (OnDisableAction != null)
+            OnDisableAction(this);
+    }
+
+    protected virtual void OnDestroyObject()
+    {
+        if (OnDestroyAction != null)
+            OnDestroyAction(this);
+    }
+
+    private void OnEnable()
+    {
+        OnEnableObject();
+    }
+
+    private void OnDisable()
+    {
+        OnDisableObject();
+    }
+
+    private void OnDestroy()
+    {
+        OnDestroyObject();
+    }
+}
---
+++ Assets/2_NewHelicopter/_RadarSystem/Scripts/RadarItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SunCube.Radar
+{
+    public class RadarItem : AdventureBehaviour
+    {
+        public RadarTargetType TargetType;
+
+        void Start()
+        {
+            if (RadarSystem.runtime != null)
+                RadarSystem.runtime.AddTarget(this);
+        }
+    }
+}
